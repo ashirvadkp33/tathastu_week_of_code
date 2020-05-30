f3c5e59128c47ed6c4e2b1827c406e4d2455dbad
Ashirvad kar pathak <59271649+ashirvadkp33@users.noreply.github.com>
2020-05-30 19:10:12 +0530

    Create Program5.py

---
+++ Program5.py
@@ -0,0 +1,9 @@
+a=float(input("run of p1"))
+b=float(input("run of p2 "))
+c=float(input("run of p3"))
+strike_rate1=(a/60)*100
+strike_rate2=(b/60)*100
+strike_rate3=(c/60)*100
+print("strike rates are: ",strike_rate1,strike_rate2,strike_rate3)
+print("new strike rates are",strike_rate1*2,strike_rate2*2,strike_rate3*2)
+print("maximum number of sixes are: ",a//6,b//6,c//6)
